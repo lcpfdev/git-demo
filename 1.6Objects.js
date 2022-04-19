e58c5e0c9b5b77325ae788f00b0d8c319ef0f939
Luka France <lukafrance@gmail.com>
2022-04-19 09:28:05 +0100

    Second Commit

--- 1.6Objects.js
+++ 1.6Objects.js
@@ -1,5 +1,5 @@
 // Activity 1
-
+/*
 const person = {
      name: "luka",
      eyes: 'blue',
@@ -15,3 +15,22 @@ const sayHi = () => {
 }
 
 sayHi()
+*/
+
+//Acitvity 2    
+/*
+const pet = {
+    name: "ezra",
+    typeOfPet: "cat",
+    age: "24",
+    color: "Orange",
+    eat: "cat food",
+    drink: "water"
+}
+
+console.log(`Your pet is eating ${pet.eat} & drinking ${pet.drink}`)
+*/
+
+// Acitivty 3 Unsure on this one !
+
+
